22ac70094dab60b681a9d2cf4c02242279b4dd5e
Thomas <gonzo113@bassfreaks.de>
2020-12-02 10:02:44 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        File bild = new File("./bild.jpg");
+
+
+        try{
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
