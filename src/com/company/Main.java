3d5ab5502895c188b3ff5cbeb8787871e918b27b
Thomas <gonzo113@bassfreaks.de>
2020-12-04 21:01:53 +0100

    update to bmp

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,28 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) {
-        File bild = new File("./bild.jpg");
+        File bild = new File("./r+m.bmp");
+        File output = new File("./copy.bmp");
 
+        BufferedInputStream bis = null;
+        BufferedOutputStream bos = null;
+
+        int bytezaehler = 0;
+        int buffer = 0;
 
         try{
+            bis = new BufferedInputStream(new FileInputStream(bild));
+            bos = new BufferedOutputStream(new FileOutputStream(output));
+
+            while((buffer = bis.read()) != -1){
+                System.out.println(buffer);
+                if(bytezaehler>40){             // Header überspringen
+                    if(buffer > 100) buffer -= 150;
+                    //if(buffer < 100) buffer += 10;
+                }
+                bytezaehler++;
+                bos.write(buffer);
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
